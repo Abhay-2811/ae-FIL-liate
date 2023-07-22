ceaceb1a90a776cdf3a6bb5e531d8119c96e0361
Ved Vaghela <vedvaghela1@gmail.com>
2023-07-22 19:40:33 +0530

    changed Verify bithched

--- client/src/components/Verify.jsx
+++ client/src/components/Verify.jsx
@@ -24,7 +24,7 @@ export const Verify = () => {
     <div class="bg-slate-900 w-screen h-screen text-slate-50 pt-32 overflow-hidden font-mono ">
         <Navbar/>
         <div class=" w-screen h-full flex flex-col items-center">
-            <div class="font-bold text-3xl">Verify Bitches</div>
+            <div class="font-bold text-3xl">Verifyium FIL-eo-Sa</div>
             <div class="w-full h-full flex flex-row justify-between pl-32 pr-32 pt-12">
             <GithubAuth handleGithubAuth={setIsGithubVerified}/>
             <IdAuth/>
